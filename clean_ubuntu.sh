fe2514d007b3d11992ef8615d3a0019e224c1770
Krabs <78506543+krabs-github@users.noreply.github.com>
2025-03-24 07:24:28 -0400

    Updates

---
+++ clean_ubuntu.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Written by Shannon B. Hill
+# Usage: fNTFY <Topic> <Title> <Tags> <Priority> <Message>
+# Description: Function to send a notification to the ntfy.krabs.me service
+
+# Variables BEGIN --------------------------------------------------------------
+vScript_Name="${0##*/}"
+vScript_Working_Directory="$($PWD)"
+vDate_Text_Display="$(date '+%m-%d-%Y')"
+vDate_Name_Compatible="$(date '+%Y%m%d')"
+vTime_Text_Display="$(date '+%r')"
+vTime_Name_Compatible="$(date '+%H%M%S')"
+vHostname="$(hostname)"
+vHost_IP_Address="$(hostname -I | awk '{print $1}')"
+vNTFY_Host="https://ntfy.krabs.me"
+# Variables END ----------------------------------------------------------------
+
+# FUNCTIONS BEGIN --------------------------------------------------------------
+fNTFY ()
+{
+    if [ -z ${1} ] || [ -z ${2} ] || [ -z ${3} ] || [ -z ${4} ] || [ -z ${5} ]
+        then
+            echo "ERROR: Missing parameter to fNTFY function"
+        else
+            curl -d "${5}" -H "Title: ${2}" -H "Priority: ${4}" -H "Tags: ${3}" ${vNTFY_Host}/${1}
+}
+
+fCleanLogs ()
+{
+    vVacuumTime="3d"
+    journalctl --vacuum-time=${vVacuumTime}
+}
+
+fAptClean ()
+{
+    apt-get auto-remove
+    apt-get clean
+}
+# FUNCTIONS END ----------------------------------------------------------------
+
+# Actions BEGIN ----------------------------------------------------------------
+fCleanLogs
+fAptClean
+# Actions END ------------------------------------------------------------------
+
+# Exit -------------------------------------------------------------------------
+Exit 0
